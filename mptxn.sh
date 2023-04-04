e3a541df126efb7e11b72e9ad16fdcc572687f24
Erick Oyugi <erick.oyugi@CreditBank.co.ke>
2023-04-04 12:24:33 +0300

    Gateway Update

--- mptxn.sh
+++ mptxn.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 today = `date +%m-%d-%Y`
-NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+NEW_UUID=$(cat /dev/urandom | tr -dc 'A-Z0-9' | fold -w 32 | head -n 1)
 ## ANSI colors (FG & BG)
 RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
 MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
 REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
 MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
-RESETBG="$(printf '\e[0m\n')"
+RESETBG="$(printf '\e[0m\n')" NOCOLOR = '\033[0m'
 
-echo -e "      * Welcome To Safaricom Mpesa Services*      "
-echo -e "      *Logged in at : $today
+
+
+# Replace the values below with your own Twilio account credentials and SMS message details
+account_sid="AC800a9847d8281896092c0c66b6441ea8"
+auth_token="ab2b11411fbe224c2eea73df7438ca17"
+from_number="+14346089725"
+to_number="+254714315046"
+message="Hello, this is a test message sent via Twilio SMS gateway."
+
+
+echo "${GREEN}**      **   ******    *******     ****      *"
+echo "** *  * **   **     *  **         *         * *"
+echo "**  **  **   ***** *   *******     ****    * * *"
+echo "**      **   **        **              *  *     *"
+echo "**      **   **        *******     ****  *       *"
+
+sleep 4
+echo -e "${WHITE}      * Welcome To Safaricom Mpesa Services*      "
+
 sleep 1
-echo "-------------------------------"
-echo "************1.MPESA********** *"
-echo "-------------------------------"
+echo "${WHITE}-------------------------------"
+echo "${GREEN}************1.MPESA********** *"
+echo "${WHITE}-------------------------------"
 sleep 2
 
-echo "Select Option 1 for MPESA";
+echo "${RED}Select Option 1 for MPESA";
 read option
 
 if [ $option  -eq 1 ]; then
+echo "${GREEN}"
 echo "1.Send Money";
 echo "";
 echo "2.Withdraw Cash";
@@ -30,42 +48,49 @@ echo "4.Lipa Na Mpesa";
 echo "";
 echo "";
 else
-echo "Invalid Option";
+echo "${RED}Invalid Option";
 exit 1;
 fi
 
 
 
 
-echo "Choose Service That You Need";
+echo "${RED}Choose Service That You Need";
 read option2
 
 if [ $option2  -eq 1 ]; then
-echo "Enter Phone Number To Send Money";
+echo "${GREEN}Enter Phone Number";
 read phoneNumber
+echo "${WHITE}-----------------";
 sleep 1
+
+echo "${GREEN}Enter  Name";
+read name
+echo "${WHITE}-----------------"
 echo "";
-echo "Enter Amount";
+echo "${GREEN}Enter Amount";
 read amount
-echo "";
+echo "${WHITE}-----------------";
 sleep 1
-echo "Enter Your Pin";
+echo "${GREEN}Enter Your Pin";
 read -s  pin
+echo "{WHITE}-----------------"
 sleep 1
 echo "";
-echo "Send Ksh.$amount.00 to $phoneNumber? To Continue Press Ok, To Cancel Press Cnc";
+echo "${RED}Send Ksh.$amount.00 to $phoneNumber? To Continue Press Ok, To Cancel Press Cnc";
 read choice
 
 if [ $choice == "Ok" ]; then
 echo "";
+
 sleep 2
 
 # Replace the values below with your own Twilio account credentials and SMS message details
-account_sid=""
-auth_token=""
-from_number="+254712345678"
-to_number="+254712345678"
-message="$NEW_UUID Confirmed.You have recieved Ksh. $amount from ERICK OYUGI 0714315046 on $today.New M_PESA balance is Ksh.47748.28.Use Lipa na MPESA Buy Goods option to payfor shopping at NO Cost"
+account_sid="AC800a9847d8281896092c0c66b6441ea8"
+auth_token="ab2b11411fbe224c2eea73df7438ca17"
+from_number="+14346089725"
+to_number="+254714315046"
+message="${WHITE}$NEW_UUID Confirmed.You have recieved Ksh. $amount from ERICK OYUGI 0714315046 on $date.New M_PESA balance is Ksh.47748.28.Use Lipa na MPESA Buy Goods option to payfor shopping at NO Cost"
 # Send the SMS message using the Twilio API
 curl -X POST "https://api.twilio.com/2010-04-01/Accounts/$account_sid/Messages.json" \
 --user "$account_sid:$auth_token" \
@@ -73,23 +98,25 @@ curl -X POST "https://api.twilio.com/2010-04-01/Accounts/$account_sid/Messages.j
 --data-urlencode "To=$phoneNumber" \
 --data-urlencode "Body=$message"
 
-
 sleep 1
-echo ""
+echo "${WHITE}"
 echo "=================================================================================================="
-echo "$NEW_UUID Confirmed. Ksh $amount.00 sent ERICK OYUGI $phoneNumber on $today. New Mpesa ";
+echo "$NEW_UUID Confirmed. Ksh $amount.00 sent $name  $phoneNumber on $date . New Mpesa ";
 echo "balance is Ksh 60,000.87.Transaction cost , Ksh 0.00. Amount you can transact within the day is";
 echo "299,900.00. Get a loan today from M-Shwari click https://mpesaapp.page.link/mshwari ";
 echo "=================================================================================================="
+
+#send this message to your mpesa thread
 fi
 if [ $choice == "Cnc" ]; then
-echo "Transaction Canceled";
+echo "${RED}Transaction Canceled";
 fi
 fi
 
+
 #Choice 4 Lipa Na Mpesa Section
 if [ $option2  -eq 4 ]; then
-echo "***********LIPA NA MPESA***********";
+echo "${WHITE}***********LIPA NA MPESA***********";
 echo "";
 sleep 1
 echo "1.Pay Bill";
@@ -106,16 +133,16 @@ echo "Enter business no";
 fi
 
 if [ $option  -eq 2 ]; then
-echo "Enter Till Number ";read tillNumber
-echo "-----------------------";
+echo "${GREEN}Enter Till Number ";read tillNumber
+echo "${WHITE}-----------------------";
 sleep 1
-echo "Enter till name";read name
-echo "-----------------------";
-echo "Enter Amount";read amount
-echo "-----------------------";
+echo "${GREEN}Enter till name";read name
+echo "${WHITE}-----------------------";
+echo "${GREEN}Enter Amount";read amount
+echo "${WHITE}-----------------------";
 sleep 1
-echo "Enter Your Pin"; read -s pin
-echo "----------------------";
+echo "${GREEN}Enter Your Pin"; read -s pin
+echo "${WHITE}----------------------";
 sleep 2
 
 echo "==================================="
@@ -132,6 +159,7 @@ echo "==================================="
 
 fi
 fi
+
 
 
 
